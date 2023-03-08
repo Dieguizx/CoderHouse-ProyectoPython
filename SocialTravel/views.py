3b3a46598f5457ba6977425d6c7b943fc7f7b157
Diego Allende <allendediego@gmail.com>
2023-03-07 23:39:37 -0300

    Formulario carga y busqueda

--- SocialTravel/views.py
+++ SocialTravel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from SocialTravel.models import Post
+from SocialTravel.forms import PostForm
 
 # Create your views here.
 
@@ -12,10 +13,31 @@ def index(request):
 
 
 def mostrar_otro_template(request):
-
     posts = Post.objects.all()
-
-
     return render(request, "SocialTravel/otro-template.html", {"posts": posts})
 
+def mostrar_post(request):
+    context = {
+        "form": PostForm(),
+        "posts": Post.objects.all()
+    }
 
+    return render(request, "SocialTravel/admin_post.html", context)
+
+def agregar_post(request):
+    post_form = PostForm(request.POST)
+    post_form.save()
+    context = {
+        "form": PostForm(),
+        "posts": Post.objects.all()
+    }
+
+    return render(request, "SocialTravel/admin_post.html", context)
+
+def buscar_post(request):
+    criterio = request.GET.get("criterio")
+    context = {
+        "posts": Post.objects.filter(carousel_caption_title__icontains=criterio).all()
+    }
+
+    return render(request, "SocialTravel/admin_post.html", context)
